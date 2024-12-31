0678ecba44093f736fb1a25eb9225ac3f3109a78
DonArchila <kevin.archila@gmail.com>
2024-12-31 16:35:05 -0700

    Add missing newline for improved readability in email sending script

--- src/08 Send an Email/my_send_email.py
+++ src/08 Send an Email/my_send_email.py
@@ -9,6 +9,7 @@ SENDER_EMAIL = 'Sender <from@example.com>'  # replace with your email address
 SENDER_USERNAME = 'b58689e2a4233d'  # replace with your email username
 SENDER_PASSWORD = '07f970d446f4f7'  # replace with your email password
 RECEIVER_EMAIL = '<Receiver to@example.com>'  # replace with receiver email address
+
 def send_email(receiver_email, subject, body):
     message = f'Subject: {subject}\n\n{body}'
     with smtplib.SMTP('smtp.mailtrap.io', 2525) as server:
