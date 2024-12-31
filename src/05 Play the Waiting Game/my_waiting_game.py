6c5b856b07920627cc1f5a7ba787a53cece68ea0
DonArchila <kevin.archila@gmail.com>
2024-12-30 23:43:19 -0700

    Refine user prompts and output formatting in waiting game implementation

--- src/05 Play the Waiting Game/my_waiting_game.py
+++ src/05 Play the Waiting Game/my_waiting_game.py
@@ -5,43 +5,47 @@ import os
 
 
 def main():
-    print("Welcome to the waiting game \n")
+    print("Welcome to the waiting game!")
     start_game()
 def start_game():
     # Generate a random number between 2 and 4
     wait_time = random.randint(2, 4)
 
-    print(f"Your task is to wait for {wait_time} seconds\n")
+    print(f"\nYour target time is {wait_time} seconds")
 
     # Start a timer after user presses enter
-    input("Press Enter to start the timer\n")
+    input(" ---Press Enter to Begin---")
     start_time = time.time()
 
     # Count seconds elapser after user presses enter again
-    input("Press enter again after you have waited for the required time")  
+    input(f"\n...Press Enter again after {wait_time} seconds...")  
     end_time = time.time()
 
     # Calculate the time taken
-    elapsed_time = round(end_time - start_time)
+    elapsed_time = end_time - start_time
+
+    print(f"\nElapsed time: {elapsed_time} seconds")
 
     # Check if the user waited for the required time
-    if elapsed_time == wait_time:
-        print("Congratulations! You waited for the correct amount of time")
+    if round(elapsed_time) == wait_time:
+        print("\nCongratulations! You waited for the correct amount of time")
+    
     # Check if the user was too slow
-    if elapsed_time > wait_time:
-        print(f"You waited for {elapsed_time} seconds.  That's {elapsed_time - wait_time} seconds too long")
+    elif elapsed_time > wait_time:
+        print(f"({elapsed_time - wait_time} seconds too long)")
+    
     # Check if the user was too fast
-    if elapsed_time < wait_time:
-        print(f"You waited for {elapsed_time} seconds.  That's {wait_time - elapsed_time} seconds too short")
+    else:
+        print(f"({wait_time - elapsed_time} seconds too fast)")
 
     while True:
-        play_again = input("Do you want to play again? (yes/no): ")
+        play_again = input("\nDo you want to play again? (yes/no): ")
         if play_again == "yes":
             start_game()
         elif play_again == "no":
-            print("Thanks for playing!")
+            print("\nThanks for playing!")
             sys.exit()
         else:
-            print("Invalid input. Please enter 'yes' or 'no'\n")
+            print("\nInvalid input. Please enter 'yes' or 'no'\n")
 if __name__ == "__main__":
     main()
